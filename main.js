f851f3449ad0e5cfd1a2092e0488e75edaf84328
unknown <midnight.qva@gmail.com>
2023-11-21 09:08:00 +1345

    Cambios en el html y css de los perfumes de hombre e implementacion de JS en el boton de pH

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const perfmanMenu = document.querySelector(".perfMan-menu");
-const perfManButton = document.querySelector("#pH");
+const perfmanButton = document.querySelector("#pH");
 const mainContainer = document.querySelector(".main-container");
 
 
 
-perfManButton.addEventListener("click", togglePerfmanMenu);
+perfmanButton.addEventListener("click", togglePerfmanMenu);
 
 
 
 
 function togglePerfmanMenu(){
     mainContainer.classList.add("inactive");
-   // perfmanMenu.classList.remove("inactive");
+    perfmanMenu.classList.remove("inactive");
 }
