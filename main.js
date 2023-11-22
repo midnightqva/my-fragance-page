05e076c62b1ca274928abe172c1489b9342d5f0e
unknown <midnight.qva@gmail.com>
2023-11-22 20:20:39 +1345

    Implementando el menu de perfumes de mujer en html y combinando con JS

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const perfmanMenu = document.querySelector(".perfMan-menu");
 const perfmanButton = document.querySelector("#pH");
 const mainContainer = document.querySelector(".main-container");
+const perfwomanMenu = document.querySelector(".perfWoman-menu");
+const perfwomanButton = document.querySelector("#pM");
+
+
+perfmanButton.addEventListener("click", showPerfmanMenu);
+perfwomanButton.addEventListener("click", showPerfwomanMenu)
 
 
 
-perfmanButton.addEventListener("click", togglePerfmanMenu);
 
-
-
-
-function togglePerfmanMenu(){
+function showPerfmanMenu(){
     mainContainer.classList.add("inactive");
     perfmanMenu.classList.remove("inactive");
+}
+
+function showPerfwomanMenu(){
+    mainContainer.classList.add("inactive");
+    perfwomanMenu.classList.remove("inactive");
 }
